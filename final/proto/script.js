2e23073d56357864dd8d638ea35dd02ba9923067
Sienna Gonzalez <sfggonzalez@ucdavis.edu>
2021-11-29 16:15:21 -0800

    user test results, changes to prototype

--- final/proto/script.js
+++ final/proto/script.js
@@ -19,6 +19,7 @@
     var majorDescription = ["joy, success, and positivity", "balance, meaning, and harmony", "new beginnings, shedding the old, and starting fresh", "upheaval, change, and sudden chaos"];
 
     // triggers fade and cards display
+
     myForm.addEventListener('submit', function(event){
         console.log("fade started");
 
@@ -29,6 +30,7 @@
     })
     
     // generates script for reading with user input
+
     myForm.addEventListener('submit', function(event){
         console.log("submit pressed");
 
@@ -117,14 +119,22 @@
 
         })
         if (firstName && adjective && desire && feeling) {
-            myText = `Hello, <a>${firstName}</a>! <br>The cards have something very <a>${adjective}</a> in store for you. I see you desire <a>${desire}</a>. I pulled the <a>${cardMinor}</a>—you’re in luck! <a>${desire}</a> is in your future. New beginnings with <a>${desire}</a> are coming soon. So you’ve been feeling <a>${feeling}</a>? It could be because of the <a>${cardMajor}</a> card. This is the card of <a>${cardMajorMeaning}</a>. Remember, this phase is temporary, so you can either enjoy it while it lasts or ride it out until it’s over! I hope you enjoyed your reading, see you next time. <br>Love,<h3> Your virtual mystic</h3>`
+            myText = `Hello, <a>${firstName}</a>!<br>
+            
+            <br>The cards have something very <a>${adjective}</a> in store for you. I see you desire <a>${desire}</a>. I pulled the <a>${cardMinor}</a>—you’re in luck! <a>${desire}</a> is in your future. New beginnings with <a>${desire}</a> are coming soon. So you’ve been feeling <a>${feeling}</a>? It could be because of the <a>${cardMajor}</a> card. This is the card of <a>${cardMajorMeaning}</a>.<br> 
+            
+            <br>Remember, this phase is temporary, so you can either enjoy it while it lasts or ride it out until it’s over! I hope you enjoyed your reading, see you next time.<br>
+            
+            <br>Love,<h3> Your virtual mystic</h3>`
         }
         else {
             myText = "Please fill all the fields so I can give you your tarot reading<br>-your virtual mystic!"
         }
         madlib.innerHTML = myText;
     });
+
     // close overlay with close button
+
     button.addEventListener('click', function(event){
         console.log("close button clicked");
 
--- usertest/script.js
+++ usertest/script.js
@@ -21,6 +21,7 @@
     var majorDescription = ["joy, success, and positivity", "balance, meaning, and harmony", "new beginnings, shedding the old, and starting fresh", "upheaval, change, and sudden chaos"];
 
     // triggers fade and cards display
+    
     myForm.addEventListener('submit', function(event){
         console.log("fade started");
 
@@ -31,6 +32,7 @@
     })
     
     // generates script for reading with user input
+
     myForm.addEventListener('submit', function(event){
         console.log("submit pressed");
 
@@ -69,6 +71,7 @@
 
         var cardMajor;
         var cardMajorMeaning;
+
         if (feeling === "excited"){
             cardMajor = majorArcana[0];
             cardMajorMeaning = majorDescription[0];
@@ -108,6 +111,7 @@
         })
 
         // triggers reading script display with overlay 
+
         readingButton.addEventListener('click', function(event){
             console.log('reading button clicked');
 
@@ -119,14 +123,17 @@
 
         })
         if (firstName && adjective && desire && feeling) {
-            myText = `Hello, <a>${firstName}</a>! <br>The cards have something very <a>${adjective}</a> in store for you. I see you desire <a>${desire}</a>. I pulled the <a>${cardMinor}</a>—you’re in luck! <a>${desire}</a> is in your future. New beginnings with <a>${desire}</a> are coming soon. So you’ve been feeling <a>${feeling}</a>? It could be because of the <a>${cardMajor}</a> card. This is the card of <a>${cardMajorMeaning}</a>. Remember, this phase is temporary, so you can either enjoy it while it lasts or ride it out until it’s over! I hope you enjoyed your reading, see you next time. <br>Love,<h3> Your virtual mystic</h3>`
+            myText = `Hello, <a>${firstName}</a>! <br>The cards have something very <a>${adjective}</a> in store for you. I see you desire <a>${desire}</a>. I pulled the <a>${cardMinor}</a> — you’re in luck! <a>${desire}</a> is in your future. New beginnings with <a>${desire}</a> are coming soon. So you’ve been feeling <a>${feeling}</a>? It could be because of the <a>${cardMajor}</a> card. This is the card of <a>${cardMajorMeaning}</a>.
+            Remember, this phase is temporary, so you can either enjoy it while it lasts or ride it out until it’s over! I hope you enjoyed your reading, see you next time. <br>Love,<h3> Your virtual mystic</h3>`
         }
         else {
             myText = "Please fill all the fields so I can give you your tarot reading<br>-your virtual mystic!"
         }
         madlib.innerHTML = myText;
     });
+
     // close overlay with close button
+
     button.addEventListener('click', function(event){
         console.log("close button clicked");
 
@@ -138,7 +145,9 @@
 
         document.getElementById("restart").style.display = "block";
     })
+
     // close overlay with escape key
+
     document.addEventListener('keydown', function(event){
 
         if (event.key === 'Escape') {
@@ -149,7 +158,9 @@
             document.getElementById("restart").style.display = "block";
         }
     })
+
     // refresh page
+
     restartButton.addEventListener('click', function(event){
         console.log("restarting");
 
